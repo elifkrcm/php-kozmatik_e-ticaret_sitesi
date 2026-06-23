9c543e63689401733ddd026aeabbf5b40cb36362
Elif karaçam <127951149+elifkrcm@users.noreply.github.com>
2026-06-23 20:59:12 +0300

    Delete gift directory

--- gift/cikis.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Tüm oturum verilerini temizler
-session_destroy(); // Oturumu sonlandırır
-header("Location:giris.php"); // Giriş sayfasına yönlendir
-exit;
-?>
